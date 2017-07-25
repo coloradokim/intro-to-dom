65bf0b8644d2937f453303748bc740c69d1f1c96
coloradokim <schlesinger.kim@gmail.com>
2017-07-24 21:50:25 -0600

    add solution

--- app.js
+++ app.js
@@ -1,20 +1,58 @@
-document.body.onload = changeListItem;
+document.body.onload = changeText;
 
 function changeText() {
-  let el = document.getElementById('about');
-  el.innerText = "About Kim Schlesinger"
+  var el = document.getElementById('about');
+  el.innerText = 'About Kim Schlesinger'
 };
 
 function changeListItem() {
-  let el = document.getElementsByClassName('list');
-  console.log(el[0].children);
+  var el = document.getElementsByClassName('list');
+  el[0].children[0].innerText = 'HIIIIIII!';
 };
-
 
 function addElement() {
   var newParagraph = document.createElement('p');
-  var newText = document.createTextNode('Howdy, g59!');
+  var newText = document.createTextNode('Hi, g59!');
 
-  newParagraph.appendChild(newText)
-  document.getElementById('greeting').appendChild(newParagraph);
-}
+  newParagraph.appendChild(newText);
+    console.log(newParagraph);
+
+  document.getElementById('greeting').appendChild(newParagraph)
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function changeText() {
+//   let el = document.getElementById('about');
+//   el.innerText = "About Kim Schlesinger"
+// };
+//
+// function changeListItem() {
+//   let el = document.getElementsByClassName('list');
+//   el[0].children[0].innerText = "HIIII"
+// };
+//
+// function addElement() {
+//   var newParagraph = document.createElement('p');
+//   var newText = document.createTextNode('Howdy, g59!');
+//
+//   newParagraph.appendChild(newText)
+//   document.getElementById('greeting').appendChild(newParagraph);
+// };
